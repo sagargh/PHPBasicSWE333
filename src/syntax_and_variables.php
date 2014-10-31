24dbc3b797dece75f4876058d2e5357c0ccd6d14
sagargh <g4maks@gmail.com>
2014-10-31 12:49:10 +0600

    This is basic for PHP syntax and variables

---
+++ src/syntax_and_variables.php
@@ -0,0 +1,27 @@
+<html>
+	<head>
+		<title>Syntax and Variables
+		</title>
+	</head>
+	<body>
+	
+	<!-- This is PHP Code -->
+	
+	<?php 
+	$name = "Sagar";
+	//echo "Hello $name";
+	echo "Hello". " ". $name;
+	?>
+	
+	<!-- HTML Tag -->
+	<br>
+	
+	<?php 
+	$firstName = "Sagar";
+	echo "Hello $firstName";
+	?>
+
+	
+	
+	</body>
+</html>
