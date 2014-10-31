2b22896c8b9e75aff843b60b577d87bbd4c4a6e6
sagargh <g4maks@gmail.com>
2014-11-01 01:59:05 +0600

    PHP Conditions

---
+++ src/conditionals.php
@@ -0,0 +1,80 @@
+<?php
+$a = 5;
+$b = 6;
+
+if ($a>$b)
+{
+	echo 'a is greater than b';
+}
+else 
+{
+	echo 'b is greater than a';
+}
+
+// b is greater than a
+
+echo '<br>';
+echo '<br>';
+$marks = 65;
+if ($marks>= 80) {
+	echo 'You got A+';
+}
+else{
+	if ($marks>= 70)
+	{
+		echo 'You got A';
+	}
+	else
+	{
+		if($marks>= 60){
+			echo 'You got A-';
+		}
+		else{
+			if($marks>= 50)
+			{
+				echo 'You got B';
+			}
+			else
+			{
+				echo 'You failed';
+			}
+		}
+	}
+}
+
+echo '<br>';
+echo '<br>';
+echo '<br>';
+
+
+$month = 'January';
+
+switch ($month) {
+	case 'January':
+	echo 'This is January month';
+	break;
+	
+	default:
+		echo 'This is another month';
+	break;
+}
+
+echo '<br>';
+echo '<br>';
+echo '<br>';
+
+$first_name = 'Alamgir';
+
+$first_name = array(
+
+		'Alamgir' => 'Its sagar\'s first name',
+		'Kabir'	  => 'Its sagar\'s last name',
+		'Sagar'   => 'Its sagar\'s nick name'
+);
+echo $first_name['Sagar'];
+
+
+
+
+
+
