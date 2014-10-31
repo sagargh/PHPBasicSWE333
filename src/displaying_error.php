107b5af0982944b994988a2547e14525c6cc85a7
sagargh <g4maks@gmail.com>
2014-10-31 18:18:06 +0600

    Diaplaying Error in the Browser

---
+++ src/displaying_error.php
@@ -0,0 +1,11 @@
+<?php
+$name = "Sagar";
+// Not inistialized $firstName variable
+//$firstName = "Alamgir";
+//Have to iitialize $firstName --- which is php.ini file
+$firstName = "Alamgir";
+echo "Hello $name";
+
+echo "<br>";
+
+echo $firstName;
