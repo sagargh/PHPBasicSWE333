6425676f1fe27d24fa1e86c8c9f8aac4dfb79e7f
sagargh <g4maks@gmail.com>
2014-10-31 19:59:06 +0600

    PHP array with vardump and printr

---
+++ src/var_dump_and_printr_array.php
@@ -0,0 +1,22 @@
+<?php
+$first_day = "Sunday";
+$second_day = "Monday";
+$third_day = "Tuesday";
+// not need to declear each variable
+// we can declear in one variable - it may be variable day
+
+$day = array('Saturday','sunday','Monday','Tuesday','Wednesday','Thursday','Firday');
+var_dump($day);
+echo '<br>';
+echo '<br>';
+print_r($day);
+echo '<br>';
+echo '<br>';
+print $day[0];
+echo '<br>';
+echo '<br>';
+print $day[1];
+
+echo '<br>';
+echo '<br>';
+print "Hello". " ".$day[6];
