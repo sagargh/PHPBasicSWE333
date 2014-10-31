c26f4131bb06ffa47b97f60f08dcde5b0d061e41
sagargh <g4maks@gmail.com>
2014-10-31 20:02:37 +0600

    PHP Array Tutorail with printr and var_damp

--- src/var_dump_and_printr_array.php
+++ src/var_dump_and_printr_array.php
@@ -7,16 +7,23 @@ $third_day = "Tuesday";
 
 $day = array('Saturday','sunday','Monday','Tuesday','Wednesday','Thursday','Firday');
 var_dump($day);
+
+//array(7) { [0]=> string(8) "Saturday" [1]=> string(6) "sunday" [2]=> string(6) "Monday" [3]=> string(7) "Tuesday" [4]=> string(9) "Wednesday" [5]=> string(8) "Thursday" [6]=> string(6) "Firday" 
 echo '<br>';
 echo '<br>';
 print_r($day);
+
+//Array ( [0] => Saturday [1] => sunday [2] => Monday [3] => Tuesday [4] => Wednesday [5] => Thursday [6] => Firday ) 
 echo '<br>';
 echo '<br>';
 print $day[0];
+//Saturday
 echo '<br>';
 echo '<br>';
 print $day[1];
-
+//sunday
 echo '<br>';
 echo '<br>';
 print "Hello". " ".$day[6];
+
+//Hello Firday
