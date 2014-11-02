f11b3c1efa6370f00a59a334ee633cc7266b4876
sagargh <g4maks@gmail.com>
2014-11-02 20:58:22 +0600

    PHP Basic with Array and Functions

---
+++ src/loops.php
@@ -0,0 +1,41 @@
+<?php
+
+//$arr = array('Touhid Bhuiyan','Mijanur Rahman','Shikha Arniban','Kaushik Sarkar');
+
+
+/* foreach ($arr as $name){
+	
+	echo $name;
+} */
+//output: Touhid BhuiyanMijanur RahmanShikha ArnibanKaushik Sarkar
+
+
+
+/* foreach ($arr as $name)
+{
+	echo '<li>'.$name.'</li>';
+} */
+/* Touhid Bhuiyan
+ Mijanur Rahman
+Shikha Arniban
+Kaushik Sarkar  [with unorder list]*/
+
+$arr = array(
+		'Head Sir' => 'Touhid Bhuyian',
+		'Senior Lecturer1' => 'Mijanur Rahman',
+		'Senior Lecturer2' => 'Shikha Anirban',
+		'Lecturer1'        => 'Kaushik Sarkar',
+		'Lecturer2'		   => 'Alamgir Kabir',	
+		'LecturerN'		   => 'Aro Oneke');
+		
+ 		
+foreach($arr as $title=>$name){
+	
+	echo "<li><strong>$title </strong> - $name </li>";
+	
+} 
+
+
+
+
+
---
+++ src/running_scripts_with_vim.php
@@ -0,0 +1,11 @@
+<?php
+
+function hello($name){
+
+	return "Hi $name !" ;
+}
+echo hello('Alamgir Kabir'); // output: Hello World
+echo '<br>';
+echo hello('there');
+
+// I want to run this script in other platform (VIM)
